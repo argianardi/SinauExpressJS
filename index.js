6414efa08e89542286cc28970e6505ecb3a6f19c
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-01 15:33:58 +0700

    express initialization

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (request, response) {
-  response.send("hello world");
-});
-//artinya jika ada yang mengakses routing '/' jalankan function berikut
-
-app.get("/about", function (request, response) {
-  response.send("this is about page");
+app.use((req, res, next) => {
+  res.status(200).json({
+    message: `Restfull nodejs dan express`,
+  });
 });
 
-app.get("/users", function (request, response) {
-  response.send("Get User");
-});
-
-app.post("/users", function (request, response) {
-  response.send("Post User");
-});
-
-app.put("/users", function (request, response) {
-  response.send("Put User");
-});
-
-app.delete("/users", function (request, response) {
-  response.send("Delete User");
-});
-
-app.listen(3000, function () {
-  console.log("server is okay");
-});
+module.exports = app;
