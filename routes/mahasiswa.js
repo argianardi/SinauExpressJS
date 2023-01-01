21f0a3313ff235b8ca4a7f10dca35cea839b1467
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-01 21:01:55 +0700

    add routing

---
+++ routes/mahasiswa.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "get method mahasiswa",
+  });
+});
+
+router.post("/", (req, res, next) => {
+  res.status(200).json({
+    message: "post method mahasiswa",
+  });
+});
+
+router.get("/:nim", (req, res, next) => {
+  const nim = req.params.nim;
+  if (nim === "12345") {
+    res.status(200).json({
+      message: "NIM 12345",
+    });
+  } else {
+    res.status(200).json({
+      message: "NIM Lain",
+    });
+  }
+});
+
+module.exports = router;
