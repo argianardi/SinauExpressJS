fbcb5d649ba3dd825d6a7a2d0a617d39553f9f23
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-02 18:57:26 +0700

    add post request use data base mysql

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -14,13 +14,19 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  const mahasiswa = {
-    nim: req.body.nim,
-    nama: req.body.nama,
-  };
-  res.status(200).json({
-    message: "post method mahasiswa",
-    data: mahasiswa,
+  const nama = req.body.nama;
+  const jurusan = req.body.jurusan;
+  var sql =
+    "INSERT INTO mahasiswa (nama, jurusan) values ('" +
+    nama +
+    "', '" +
+    jurusan +
+    "')";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(200).json({
+      message: "Data mahasiswa berhasil ditambahkan",
+    });
   });
 });
 
